4ccfc4539eb792d50bb6613a314518261c877c15
Litisha Mohapatra <60422293+litisha22@users.noreply.github.com>
2025-09-04 23:19:27 +0530

    Update step01_crime_scene_report.sql

--- step01_crime_scene_report.sql
+++ step01_crime_scene_report.sql
@@ -1,3 +1,11 @@
+-- Step 1: Analyze the Crime Scene Report
+-- ---------------------------------------
+-- We begin by locating the initial crime report to understand:
+-- - When and where the crime occurred
+-- - Any hints about witnesses or the event itself
+-- 
+-- This helps us narrow down the scope of investigation to a specific event.
+
 SELECT * FROM crime_scene_report
 WHERE lower(type) = 'murder' 
 AND date = '20180115' 
