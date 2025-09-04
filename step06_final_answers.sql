b52d83241065befc92a6ae6f370d3860544adb99
Litisha Mohapatra <60422293+litisha22@users.noreply.github.com>
2025-09-05 00:16:38 +0530

    Create step06_final_answers.sql

---
+++ step06_final_answers.sql
@@ -0,0 +1,15 @@
+-- Step 6: Submit the Final Answers
+-- ---------------------------------
+-- We now submit the two key individuals involved in the crime:
+-- 1. The murderer: Jeremy Bowers
+-- 2. The mastermind: Miranda Priestly
+--
+-- These inserts simulate a final check in the challenge platform.
+
+-- Murderer
+INSERT INTO solution VALUES (1, 'Jeremy Bowers');
+SELECT value FROM solution;
+
+-- Mastermind
+INSERT INTO solution VALUES (1, 'Miranda Priestly');
+SELECT value FROM solution;
